1cb7210843a538a267b3fb47a434419d573299c9
JanathanPlanas <59850939+JanathanPlanas@users.noreply.github.com>
2024-01-07 19:14:11 -0300

    Template Project DataScience

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Benefícios do uso de softwares ou aplicações inteligente de DeepLearning para previsão de feixes de dados usando o ambiente, câmera e GPS com intuito de orientar melhor a direção dos feixes de dados e garantir melhor comunicação,  tendo em vista que lidar com dados em mmWave e sub-terahertz torna-se mais necessário e maior custo de treinamento para métodos tradicionais de comunicação',
+    author='JanathanJunior/UFC-Universidade_Federal_Ceara/Orientador:YuriCarvalho',
+    license='',
+)
